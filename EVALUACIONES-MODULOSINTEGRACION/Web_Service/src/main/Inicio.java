48a9b76ada9fd7e768b3a61342af6134ed8495ea
daniela-OLIPC\daniela <emmalejandra@gmail.com>
2017-12-28 11:13:19 -0300

    Se sube actividad guía 1.2 consumo web service

---
+++ EVALUACIONES-MODULOSINTEGRACION/Web_Service/src/main/Inicio.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import Vista.Indicadores;
+import Vista.Inicial;
+import Vista.VerIndicador;
+
+/**
+ *
+ * @author Concepción
+ */
+public class Inicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Indicadores i = new Indicadores();
+        i.setVisible(true);*/
+        Inicial i = new Inicial();
+        i.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
