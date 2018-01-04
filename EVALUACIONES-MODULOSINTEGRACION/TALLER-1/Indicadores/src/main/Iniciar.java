5f256c00e3e1a4487580b49edb7454eff6ec2a84
daniela-OLIPC\daniela <emmalejandra@gmail.com>
2018-01-04 18:49:47 -0300

    Se sube taller 1 modulo de integración-presenta errore de comparar valores absolutos de variacion por dia -se repiten días con valor mínimo y máximo

---
+++ EVALUACIONES-MODULOSINTEGRACION/TALLER-1/Indicadores/src/main/Iniciar.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import Vista.Indicador;
+
+/**
+ *
+ * @author Concepción
+ */
+public class Iniciar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Indicador i = new Indicador();
+        i.setVisible(true);
+    }
+    
+}
